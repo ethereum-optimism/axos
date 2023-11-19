45f8943cac44da161b326c5f471360b8fe52cf87
refcell <abigger87@gmail.com>
2023-11-18 16:18:22 -0800

    chore(primitives): attributes derivation and tests

--- crates/primitives/src/attributes.rs
+++ crates/primitives/src/attributes.rs
@@ -36,27 +36,18 @@ pub struct AttributesDepositedCall {
 impl TryFrom<Vec<u8>> for AttributesDepositedCall {
     type Error = anyhow::Error;
 
-    fn try_from(_value: Vec<u8>) -> anyhow::Result<Self> {
-        Err(anyhow::anyhow!("Not implemented"))
-        // let number = u64::from_le_bytes(value[0..8].try_into()?);
-        // let timestamp = u64::from_le_bytes(value[8..16].try_into()?);
-        // let basefee = U256::from_little_endian(&value[16..48]);
-        // let hash = B256::from_little_endian(&value[48..80]);
-        // let sequence_number = u64::from_le_bytes(value[80..88].try_into()?);
-        // let batcher_hash = B256::from_little_endian(&value[88..120]);
-        // let fee_overhead = U256::from_little_endian(&value[120..152]);
-        // let fee_scalar = U256::from_little_endian(&value[152..184]);
-        //
-        // Ok(Self {
-        //     number,
-        //     timestamp,
-        //     basefee,
-        //     hash,
-        //     sequence_number,
-        //     batcher_hash,
-        //     fee_overhead,
-        //     fee_scalar,
-        // })
+    fn try_from(value: Vec<u8>) -> anyhow::Result<Self> {
+        let call = SetL1BlockValuesCall::try_from(value)?;
+        Ok(Self {
+            number: call.get_block_number()?,
+            timestamp: call.get_block_timestamp()?,
+            basefee: call.get_basefee()?,
+            hash: call.get_block_hash()?,
+            sequence_number: call.get_sequence_number()?,
+            batcher_hash: call.get_batcher_hash()?,
+            fee_overhead: call.get_l1_fee_overhead()?,
+            fee_scalar: call.get_l1_fee_scalar()?,
+        })
     }
 }
 
@@ -86,22 +77,34 @@ mod tests {
     const TEST_CALLDATA: [u8; 260] = hex!(
         "015d8eb900000000000000000000000000000000000000000000000000000000008768240000000000000000000000000000000000000000000000000000000064443450000000000000000000000000000000000000000000000000000000000000000e0444c991c5fe1d7291ff34b3f5c3b44ee861f021396d33ba3255b83df30e357d00000000000000000000000000000000000000000000000000000000000000050000000000000000000000007431310e026b69bfc676c0013e12a1a11411eec9000000000000000000000000000000000000000000000000000000000000083400000000000000000000000000000000000000000000000000000000000f4240"
     );
+    const BLOCK_HASH: B256 =
+        b256!("0444c991c5fe1d7291ff34b3f5c3b44ee861f021396d33ba3255b83df30e357d");
+    const BATCHER_HASH: B256 =
+        b256!("0000000000000000000000007431310e026b69bfc676c0013e12a1a11411eec9");
+
+    fn check_attributes(attributes: &AttributesDepositedCall) {
+        assert_eq!(attributes.number, 8874020);
+        assert_eq!(attributes.timestamp, 1682191440);
+        assert_eq!(attributes.basefee, U256::from(14u64));
+        assert_eq!(attributes.hash, BLOCK_HASH);
+        assert_eq!(attributes.sequence_number, 5);
+        assert_eq!(attributes.batcher_hash, BATCHER_HASH);
+        assert_eq!(attributes.fee_overhead, U256::from(2100u64));
+        assert_eq!(attributes.fee_scalar, U256::from(1000000u64));
+    }
 
     #[test]
     fn test_attributes_from_byte_slice() {
         let call = AttributesDepositedCall::try_from(&TEST_CALLDATA[..]);
         let call = call.unwrap();
-        let expected_hash =
-            b256!("0444c991c5fe1d7291ff34b3f5c3b44ee861f021396d33ba3255b83df30e357d");
-        let batcher_hash =
-            b256!("0000000000000000000000007431310e026b69bfc676c0013e12a1a11411eec9");
-        assert_eq!(call.number, 8874020);
-        assert_eq!(call.timestamp, 1682191440);
-        assert_eq!(call.basefee, U256::from(14u64));
-        assert_eq!(call.hash, expected_hash);
-        assert_eq!(call.sequence_number, 5);
-        assert_eq!(call.batcher_hash, batcher_hash);
-        assert_eq!(call.fee_overhead, U256::from(2100u64));
-        assert_eq!(call.fee_scalar, U256::from(1000000u64));
+        check_attributes(&call);
+    }
+
+    #[test]
+    #[cfg(feature = "alloc")]
+    fn test_attributes_from_vec() {
+        let call = AttributesDepositedCall::try_from(TEST_CALLDATA.to_vec());
+        let call = call.unwrap();
+        check_attributes(&call);
     }
 }
--- crates/primitives/src/head.rs
+++ crates/primitives/src/head.rs
@@ -40,3 +40,45 @@ impl TryFrom<BlockWithTransactions> for HeadInfo {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::Transaction;
+    use alloy_primitives::{b256, hex, B256};
+
+    const TEST_CALLDATA: [u8; 260] = hex!(
+        "015d8eb900000000000000000000000000000000000000000000000000000000008768240000000000000000000000000000000000000000000000000000000064443450000000000000000000000000000000000000000000000000000000000000000e0444c991c5fe1d7291ff34b3f5c3b44ee861f021396d33ba3255b83df30e357d00000000000000000000000000000000000000000000000000000000000000050000000000000000000000007431310e026b69bfc676c0013e12a1a11411eec9000000000000000000000000000000000000000000000000000000000000083400000000000000000000000000000000000000000000000000000000000f4240"
+    );
+    const BLOCK_HASH: B256 =
+        b256!("0444c991c5fe1d7291ff34b3f5c3b44ee861f021396d33ba3255b83df30e357d");
+
+    #[test]
+    #[cfg(feature = "alloc")]
+    fn test_head_info_try_from() {
+        use alloc::vec;
+
+        let block = BlockWithTransactions {
+            hash: BLOCK_HASH,
+            number: 8874020,
+            parent_hash: B256::default(),
+            timestamp: 1682191440,
+            transactions: vec![Transaction {
+                hash: B256::default(),
+                data: TEST_CALLDATA.to_vec(),
+                ..Default::default()
+            }],
+        };
+
+        let head_info = HeadInfo::try_from(block).unwrap();
+
+        assert_eq!(head_info.l2_block_info.hash, BLOCK_HASH);
+        assert_eq!(head_info.l2_block_info.number, 8874020);
+        assert_eq!(head_info.l2_block_info.parent_hash, B256::default());
+        assert_eq!(head_info.l2_block_info.timestamp, 1682191440);
+        assert_eq!(head_info.l1_epoch.number, 8874020);
+        assert_eq!(head_info.l1_epoch.hash, BLOCK_HASH);
+        assert_eq!(head_info.l1_epoch.timestamp, 1682191440);
+        assert_eq!(head_info.sequence_number, 5);
+    }
+}
