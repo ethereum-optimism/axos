f2b0d37fa05e13097873766eede852c99c236017
refcell <abigger87@gmail.com>
2023-11-21 09:53:41 -0800

    feat(engine): Engine API

--- crates/axos/src/lib.rs
+++ crates/axos/src/lib.rs
@@ -7,7 +7,7 @@
 )]
 #![deny(unused_must_use, rust_2018_idioms)]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
-#![no_std]
+#![cfg_attr(not(feature = "std"), no_std)]
 
 #[cfg(feature = "alloc")]
 extern crate alloc;
@@ -15,6 +15,7 @@ extern crate alloc;
 /// Re-export [axos_primitives] for convenience.
 pub use axos_primitives::*;
 
+pub mod engine;
 pub mod info;
 pub mod ingest;
 pub mod stages;
